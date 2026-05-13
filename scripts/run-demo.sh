d88a5ddd24514f03be298e5a0c84f1bc059e3cbe
Riccardo Valtorta <riccardo.valtorta@vhit-weifu.com>
2026-05-13 10:03:09 +0200

    Demo of a consumer app

--- scripts/run-demo.sh
+++ scripts/run-demo.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-source $SNAP/usr/bin/setup-env.sh
+export MOVEIT_RUNTIME=$SNAP/moveit-runtime
+
+source $MOVEIT_RUNTIME/usr/bin/setup-env.sh
 
 exec $ROS_BASE/opt/ros/humble/bin/ros2 launch \
   moveit_resources_panda_moveit_config \
